b035678d0820f425dd36a1e2e4c010ed99e4e914
Shreehari-6525 <148474675+Shreehari-6525@users.noreply.github.com>
2026-01-13 10:23:29 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,19 @@
+
+
+print("===================PERSONAL INTRO=======================")
+
+name = input("What is Your Name? : ")
+age = input("How old are you? : ")
+hobby = input("What are your hobbies (seperated by ',')? : ")
+education = input("What kind of Education you are pursuing? : ")
+city = input("Tell me name of your city: ")
+
+print("-----------------------------------------------------------\n\n")
+
+print(f"                 🎉 Welcome {name} 🎉")
+print(f"                    You are {age} Years old..")
+print(f"                  You Love {hobby}")
+print(f"         It's great to know that you're from {city}")
+print(f"also glad to know that you're currently learning {education}")
+print("---------------------------------------------------------------------")
+print(f"           Have a Wonderful day dear {name} 😊\n\n")
